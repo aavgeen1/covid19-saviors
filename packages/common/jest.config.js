f47c30f999aed49dfd2e21d3af52a37f44b3d123
aavgeen1 <aavgeensingh1@gmail.com>
2020-04-19 03:15:52 +0530

    :wrench: Adding configs and packages and scripts

---
+++ packages/common/jest.config.js
@@ -0,0 +1,12 @@
+module.export = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '\\.(js|jsx)?$': 'babel-jest',
+  },
+  testMatch: ['<rootDir>/src/**/>(*.)spec.{js, jsx}'], // finds test
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node' ],
+  testPathIgnorePatterns: ['/node_modules/', '/public/'],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-dom/extend-expect', 
+  ] // setupFiles before the tests are ran
+};
